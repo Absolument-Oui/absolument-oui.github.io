3ca4b699a04742a61ddc5265b091a7faa727c3ee
LeVraiStagiaire <f.goussot@outlook.com>
2025-05-09 22:05:39 +0200

    Add initial release notes for V3.1 of InfoGare with migration details and future updates

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -6,6 +6,6 @@ export default navbar([
     text: "InfoGare",
     icon: "pen",
     prefix: "/posts/InfoGare/",
-    children: ["V3.md", "V3.1-1.md"],
+    children: ["V3.md", "V3.1-1.md", "V3.1-2.md"],
   },
 ]);
